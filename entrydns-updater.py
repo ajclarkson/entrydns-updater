5eb6a830b612d80ee812084221206f38c7b6ebd6
Adam Clarkson <adam@ajclarkson.co.uk>
2014-12-05 16:50:17 +0000

    General Documentation Update

--- entrydns-updater.py
+++ entrydns-updater.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
-# entrydns-updater.py ~ ajclarkson.co.uk
-#
-# Updater for Dynamic DNS on EntryDNS Domains
-# Performs an update for each given domain access token in
-# the hosts.json file.
+'''
+entrydns-updater.py ~ ajclarkson.co.uk
 
+Updater for Dynamic DNS on EntryDNS Domains
+Performs an update for each given domain access token in
+the hosts.json file.
+'''
 import json
 from urllib2 import urlopen
 import requests
@@ -18,7 +19,8 @@ def get_cached_ip():
 	Retrieve Cached IP From File, cuts down on API requests to EasyDNS if
 	IP Address hasn't changed.
 
-	Return: (string) Cached IP or 0 to force refresh of public IP
+	Returns: 
+		cached_ip: Cached IP or 0 to force refresh of public IP
 	'''
 	try:
 		cached_file = open(SCRIPT_PATH + '.entrydns-cachedip', 'r')
@@ -32,7 +34,8 @@ def set_cached_ip(ip):
 	'''
 	Stores IP Address in the Cached
 
-	ip: (string) Address to be Cached
+	Args:
+		ip: Address to be Cached
 	'''
 	try:
 		cached_file = open(SCRIPT_PATH + '.entrydns-cachedip', 'w')
@@ -45,7 +48,8 @@ def get_ip():
 	'''
 	Retrieves public IP (from httpbin) with cached IP and returns import
 
-	Return: (string) Public IP
+	Returns:
+		Public IP as a string
 	'''
 	public_ip = json.load(urlopen('http://httpbin.org/ip'))['origin']
 	return public_ip
@@ -54,7 +58,11 @@ def load_hosts():
 	'''
 	Loads the hosts.json file containing access tokens for EasyDNS
 
-	Return: (dict) Hosts and Access Tokens
+	Returns: 
+		A dictionary of hosts and access tokens, e.g 
+
+		{'example-host':'678dxjvid928skf',
+		 'example-host2':'8299fd0as88fd8d'}
 	'''
 	try:
 		hosts_file = open(SCRIPT_PATH + 'hosts.json', 'r')
@@ -67,10 +75,12 @@ def update_host(token, current_ip):
 	'''
 	Formulate and Execute an Update request on EntryDNS API for a given access token / IP
 
-	token: (string) Access Token for an EntryDNS Domain
-	current_ip: (string) IP to point EasyDNS Domain to
+	Args:
+		token: (string) Access Token for an EntryDNS Domain
+		current_ip: (string) IP to point EasyDNS Domain to
 
-	Return: Status (Either OK, or Error + Code)
+	Returns: 
+		Status (Either OK, or Error + Code)
 	'''
 	url = 'https://entrydns.net/records/modify/%s' % token
 	payload = 'ip=%s' % current_ip
@@ -83,9 +93,6 @@ def update_host(token, current_ip):
 current_ip = get_ip()
 cached_ip = get_cached_ip()
 if cached_ip != current_ip:
-	'''
-	If current IP is different to cached IP, then hosts need updating
-	'''
 	set_cached_ip(current_ip)
 	hosts = load_hosts()
 	for host in hosts:
@@ -93,7 +100,3 @@ if cached_ip != current_ip:
 		print "%s -- Updating %s: %s" % (strftime("%Y-%m-%d %H:%M:%S"),host, result)
 else:
 	print "%s -- Public IP Matches Cache (%s), Nothing to Do..." % (strftime("%Y-%m-%d %H:%M:%S"), current_ip)
-
-
-
-
